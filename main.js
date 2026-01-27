acd44b2b7beb8f8fc03e5b7a7cd1c2cf3112a2c1
Kagumi <adrienjuttaud@gmail.com>
2026-01-27 14:16:36 +0100

    final init

---
+++ main.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port localhost:${PORT}`);
+});
